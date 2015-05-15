cf723d1bfff46cd974fea7f747c897c161e19a35
MarQuis Knox <github@marquisknox.com>
2015-05-15 15:38:24 +0200

    FIXED:  truncate.sql

--- scripts/destroy/db/truncate.sql
+++ scripts/destroy/db/truncate.sql
@@ -1,6 +1,6 @@
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE `cloneui_phrase`, `cloneui_site_config`, `cloneui_site_theme`, `cloneui_usergroup_member`, `cloneui_usergroup_permission`, `cloneui_user_confirm`, `cloneui_usergroup`, `cloneui_site_permission`, `cloneui_banned_email`, `cloneui_banned_ip`, `cloneui_user`, `cloneui_language`;  
-DROP TABLE `cloneui_metadata` ;
-DROP TABLE `cloneui_oauth_token` ;
-DROP TABLE `cloneui_oauth_app` ;
+DROP TABLE `jzxpr_phrase`, `jzxpr_site_config`, `jzxpr_site_theme`, `jzxpr_usergroup_member`, `jzxpr_usergroup_permission`, `jzxpr_user_confirm`, `jzxpr_usergroup`, `jzxpr_site_permission`, `jzxpr_banned_email`, `jzxpr_banned_ip`, `jzxpr_user`, `jzxpr_language`;  
+DROP TABLE `jzxpr_metadata` ;
+DROP TABLE `jzxpr_oauth_token` ;
+DROP TABLE `jzxpr_oauth_app` ;
 SET FOREIGN_KEY_CHECKS = 1;
